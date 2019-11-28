a130d7ac27547bd97c6a49a3fec3bc55c89540f0
Peter Moser <p.moser@noi.bz.it>
2019-11-28 16:32:48 +0100

    Ninja: Remove INVALID tag in swagger UI

--- ninja/src/main/java/it/bz/idm/bdp/ninja/controller/SwaggerController.java
+++ ninja/src/main/java/it/bz/idm/bdp/ninja/controller/SwaggerController.java
@@ -1,16 +1,27 @@
 package it.bz.idm.bdp.ninja.controller;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import springfox.documentation.annotations.ApiIgnore;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
 
 @ApiIgnore
 @Controller
 public class SwaggerController {
 
-    @RequestMapping("/")
-    public String swaggerUi() {
-        return "redirect:/swagger-ui.html";
-    }
+	@RequestMapping("/")
+	public String swaggerUi() {
+		return "redirect:/swagger-ui.html";
+	}
+
+	@Bean
+	UiConfiguration uiConfig() {
+		return UiConfigurationBuilder
+				.builder()
+				.validatorUrl(null)
+				.build();
+	}
 }
